bdf9525c2531a1d22d045ce59929120329d43e82
dpx2k1 <sk8_7506@hotmail.com>
2015-05-30 14:49:32 -0400

    JSON

--- src/Modele/User.java
+++ src/Modele/User.java
@@ -1,6 +1,9 @@
 package Modele;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.*;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -8,18 +11,21 @@ import java.util.*;
  */
 //Information sur l'utilisateur
 public class User {
-    
+
     //Données membres de l'utilisateur
     private String nom, prenom, numeroRue, nomRue, codePostal, province, pays,
             password;
-    
+
     private ArrayList informationUser;
-    
+    //Objet JSON pour la database
+    private JSONObject obj;
+
     //Constructeur
-    public User() {
+    public User() throws IOException {
         init();
+        exportUser();
     }
-    
+
     //Initialisation des données membres
     public void init() {
         setNom(nom);
@@ -36,31 +42,31 @@ public class User {
     public void setNom(String unNom) {
         this.nom = unNom;
     }
-    
+
     public void setPrenom(String unPrenom) {
         this.prenom = unPrenom;
     }
-    
+
     public void setNumeroRue(String unNumeroRue) {
         this.numeroRue = unNumeroRue;
     }
-    
+
     public void setNomRue(String unNomRue) {
         this.nomRue = unNomRue;
     }
-    
+
     public void setCodePostal(String unCodePostal) {
         this.codePostal = unCodePostal;
     }
-    
+
     public void setProvince(String uneProvince) {
         this.province = uneProvince;
     }
-    
+
     public void setPays(String unPays) {
         this.pays = unPays;
     }
-    
+
     public void setPassword(String newPassword) {
         this.password = newPassword;
     }
@@ -69,37 +75,37 @@ public class User {
     public String getNom() {
         return this.nom;
     }
-    
+
     public String getPrenom() {
         return this.prenom;
     }
-    
+
     public String getNumeroRue() {
         return this.numeroRue;
     }
-    
+
     public String getNomRue() {
         return this.nomRue;
     }
-    
+
     public String getCodePostal() {
         return this.codePostal;
     }
-    
+
     public String getProvince() {
         return this.province;
     }
-    
+
     public String getPays() {
         return this.pays;
     }
-    
+
     public String getPassword() {
         return this.password;
     }
-    
+
     //Ajoute les informations dans une ArrayList
-    public ArrayList getListeInfo(){
+    public ArrayList getListeInfo() {
         informationUser = new ArrayList<>();
         informationUser.add(getNom());
         informationUser.add(getPrenom());
@@ -109,5 +115,23 @@ public class User {
         informationUser.add(getProvince());
         informationUser.add(getPays());
         return informationUser;
+    }
+
+    //Write les info du user avec JSON
+    public void exportUser() throws IOException {
+        obj = new JSONObject();
+
+        obj.put("nom", getNom());
+        obj.put("prenom", getPrenom());
+        obj.put("numeroRue", getNumeroRue());
+        obj.put("nomRue", getNomRue());
+        obj.put("codePostal", getCodePostal());
+        obj.put("province", getProvince());
+        obj.put("pays", getPays());
+        obj.put("password", getPassword());
+
+        //Write l'objet JSON
+        StringWriter out = new StringWriter();
+        obj.writeJSONString(out);
     }
 }
